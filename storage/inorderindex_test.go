a31fd504341f7f3c24cb2adc873e6f8899511ffc
aoligei <zhangshurui1999@163.com>
2020-11-12 23:28:39 +0800

    oh so very simplified idiot version of my first test.go

--- storage/inorderindex_test.go
+++ storage/inorderindex_test.go
@@ -1,2 +1,31 @@
 package storage
 
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFromIndexOnLevel(t *testing.T) {
+	v := FromIndexOnLevel(3, 3)
+	if v == 55 {
+		fmt.Println("奥利给！")
+	}
+}
+
+func TestFromLeafIndex(t *testing.T) {
+	v := FromLeafIndex(3)
+	if v == 6 {
+		fmt.Println("奥利给！")
+	}
+}
+
+func TestFromPostorder(t *testing.T) {
+	v := FromPostorder(125)
+	if v == 95 {
+		fmt.Println("奥利给！")
+	}
+}
+
+func TestPostorder(t *testing.T) {
+
+}
