d8aa1bfbf8479576ecb354b4c95ad278e3128cd8
Frank <35358771+Frankonly@users.noreply.github.com>
2020-11-19 11:19:09 +0800

    server for upchain

---
+++ data/data.go
@@ -0,0 +1,27 @@
+// Package data provides convenience routines to access files in the data
+// directory.
+package data
+
+import (
+	"path/filepath"
+	"runtime"
+)
+
+// basePath is the root directory of this package.
+var basePath string
+
+func init() {
+	_, currentFile, _, _ := runtime.Caller(0)
+	basePath = filepath.Dir(currentFile)
+}
+
+// Path returns the absolute path the given relative file or directory path,
+// relative to the google.golang.org/grpc/examples/data directory in the
+// user's GOPATH.  If rel is already absolute, it is returned unmodified.
+func Path(rel string) string {
+	if filepath.IsAbs(rel) {
+		return rel
+	}
+
+	return filepath.Join(basePath, rel)
+}
--- server/main.go
+++ server/main.go
@@ -1,1 +1,65 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"upchain/api"
+	pb "upchain/api/accumulator"
+	"upchain/data"
+	"upchain/storage"
+)
+
+var (
+	tls      = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile = flag.String("cert_file", "", "The TLS cert file")
+	keyFile  = flag.String("key_file", "", "The TLS key file")
+	dbFile   = flag.String("db_file", "", "The upchain DB file")
+	port     = flag.Int("port", 10000, "The server port")
+)
+
+func main() {
+	flag.Parse()
+
+	db, err := storage.NewLevelDB(data.Path(*dbFile))
+	if err != nil {
+		log.Fatalf("failed to initialize db: %v", err)
+	}
+
+	merkle, err := storage.NewMerkleTreeStreaming(db)
+	if err != nil {
+		log.Fatal("failed to initialize merkle accumulator: %v", err)
+	}
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+	if *tls {
+		if *certFile == "" {
+			*certFile = data.Path("x509/server_cert.pem")
+		}
+		if *keyFile == "" {
+			*keyFile = data.Path("x509/server_key.pem")
+		}
+		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		if err != nil {
+			log.Fatalf("Failed to generate credentials %v", err)
+		}
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
+	}
+
+	grpcServer := grpc.NewServer(opts...)
+	apiServer := api.NewServer(merkle)
+	pb.RegisterAccumulatorServer(grpcServer, apiServer)
+
+	err = grpcServer.Serve(lis)
+	log.Println(err)
+}
